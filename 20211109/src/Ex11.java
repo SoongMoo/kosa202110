943455b76ad315165ca0b9eab37d7a673f23d992
user <user@DESKTOP-4NMVE1B>
2021-11-09 14:58:27 +0900

    202111091500

--- 20211109/src/Ex11.java
+++ 20211109/src/Ex11.java
@@ -1,33 +1,21 @@
 
-public class Ex10 {
+public class Ex11 {
 	String company;
 	String model;
 	String color;
 	int maxSpeed;
-	public Ex10(String company, String model, 
+	public Ex11() {}
+	public Ex11(String company, String model, 
 			int maxSpeed) {
-		// 생성자에서 다른 생성자 실행
-		this(company,model);
-		/*
 		this.company = company;
 		this.model = model;
-		*/
 		this.maxSpeed = maxSpeed;
 	}
-	public Ex10(String company, String model, 
+	public Ex11(String company, String model, 
 			String color) {
-		this(company,model);//this()메서드로 생성자 호출
-		/*
 		this.company = company;
 		this.model = model;
-		*/
 		this.color = color;
-	}
-	// this는 멤버필드이다.
-	//this()메서드를 사용되는 생성자는 아래에 만들어 주는 것이 좋다.
-	public Ex10(String company, String model) {
-		this.company = company;
-		this.model = model;
 	}
 	public String getCompany() {
 		return company;
@@ -52,5 +40,5 @@ public class Ex10 {
 	}
 	public void setMaxSpeed(int maxSpeed) {
 		this.maxSpeed = maxSpeed;
-	}
+	}	
 }

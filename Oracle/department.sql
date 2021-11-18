04ea735b874d25cc048a53982a31c91e7a40204b
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2021-11-18 17:01:14 +0900

    Add files via upload

---
+++ Oracle/department.sql
@@ -0,0 +1,191 @@
+INSERT INTO departments VALUES 
+        ( 10
+        , 'Administration'
+        , 200
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 20
+        , 'Marketing'
+        , 201
+        , 1800
+        );
+
+------------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO departments VALUES 
+        ( 30
+        , 'Purchasing'
+        , 114
+        , 1700
+	);
+                
+INSERT INTO departments VALUES 
+        ( 40
+        , 'Human Resources'
+        , 203
+        , 2400
+        );
+
+INSERT INTO departments VALUES 
+        ( 50
+        , 'Shipping'
+        , 121
+        , 1500
+        );
+                
+INSERT INTO departments VALUES 
+        ( 60 
+        , 'IT'
+        , 103
+        , 1400
+        );
+                
+INSERT INTO departments VALUES 
+        ( 70 
+        , 'Public Relations'
+        , 204
+        , 2700
+        );
+                
+INSERT INTO departments VALUES 
+        ( 80 
+        , 'Sales'
+        , 145
+        , 2500
+        );
+                
+INSERT INTO departments VALUES 
+        ( 90 
+        , 'Executive'
+        , 100
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 100 
+        , 'Finance'
+        , 108
+        , 1700
+        );
+                
+INSERT INTO departments VALUES 
+        ( 110 
+        , 'Accounting'
+        , 205
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 120 
+        , 'Treasury'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 130 
+        , 'Corporate Tax'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 140 
+        , 'Control And Credit'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 150 
+        , 'Shareholder Services'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 160 
+        , 'Benefits'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 170 
+        , 'Manufacturing'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 180 
+        , 'Construction'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 190 
+        , 'Contracting'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 200 
+        , 'Operations'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 210 
+        , 'IT Support'
+        , NULL
+        , 1700
+        );
+
+
+INSERT INTO departments VALUES 
+        ( 220 
+        , 'NOC'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 230 
+        , 'IT Helpdesk'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 240 
+        , 'Government Sales'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 250 
+        , 'Retail Sales'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 260 
+        , 'Recruiting'
+        , NULL
+        , 1700
+        );
+
+INSERT INTO departments VALUES 
+        ( 270 
+        , 'Payroll'
+        , NULL
+        , 1700
+        );

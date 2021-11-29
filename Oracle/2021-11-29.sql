8df091b747c90d7e68db3c94b33c289fbc5237e5
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2021-11-29 14:49:22 +0900

    Add files via upload

--- Oracle/2021-11-29.sql
+++ Oracle/2021-11-29.sql
@@ -323,6 +323,128 @@ END;
 /
 
 
+--- 급여를 1000으로 나눈 몫이 3이하면 사원 
+--                          5이하면 주임
+--                          7이하면 대리
+--                          9이하면 과장
+--                          10이하면 차장
+--                          13이하면 부장
+--                          15이하면 본부장
+--                          그외엔 이사 로 출력하세요
+-- 를 프로시져로 만들어서 사용하세요.
+-- case 문으로
+
+
+DECLARE 
+    empid emp.employee_id%type := 130; 
+    grade varchar2(20); 
+BEGIN
+    select
+    case when trunc(salary / 1000) <= 3 then '사원'
+             when trunc(salary / 1000) between 4 and 5 then '주임'
+             when trunc(salary / 1000) <= 7 then '대리'
+             when trunc(salary / 1000) <= 9 then '과장'
+             when trunc(salary / 1000) <= 10 then '차장'
+             when trunc(salary / 1000) <= 13 then '부장'
+             when trunc(salary / 1000) <= 15 then '본부장'
+             else '이사' end
+    into grade
+    from emp
+    where employee_id = empid;
+    DBMS_OUTPUT.put_line('직원의 직책은 : ' || grade);
+end;
+/
+
+DECLARE
+    grade CHAR(1)  := UPPER('&grade');
+    result1  VARCHAR2(30);
+BEGIN
+    result1 :=
+    case grade 
+        when 'A' then '90점 이상입니다.'
+        when 'B' then '80점 이상입니다.'
+        when 'C' then '70점 이상입니다.'
+        when 'D' then '60점 이상입니다.'
+        when 'F' then '60점 미만입니다.'
+    end;
+    DBMS_OUTPUT.PUT_LINE( 'grade : ' || grade || ' 일 때 ' || result1) ; 
+end;
+/
+
+DECLARE 
+    empid emp.employee_id%type := &empid; 
+    sal number;
+    result1 varchar2(30);
+BEGIN
+    SELECT trunc(salary / 1000) 
+    INTO sal
+    FROM employees
+    WHERE employee_id = empid; 
+    /* case로 변경하시오 */
+    result1 :=
+    case when sal <= 3 then '사원'
+        when sal <= 5 then  '주임'
+        when sal <= 7 then '대리'
+        when sal <= 9 then '과장'
+        when sal <= 10 then '차장'
+        when sal <= 13 then '부장'
+        when sal <= 15 then '본부장'
+        else '이사'
+    end;        
+    DBMS_OUTPUT.PUT_LINE('직원의 직책은 : ' || result1 || '입니다');
+END;
+/
+
+DECLARE
+    grade CHAR(1) := UPPER('&grade');
+BEGIN
+    case 
+        when grade = 'A' THEN DBMS_OUTPUT.PUT_LINE('Excellent');
+        when grade in ('B','C') THEN DBMS_OUTPUT.PUT_LINE('Good');
+        else DBMS_OUTPUT.PUT_LINE('No such grade');
+    end case;
+end;
+/
+---- 부서장번호를 입력 받아서 부서장의 부서번호와 부서이름을 가져오고
+--- 그 부서의 사원수도 가지고 오세요. 
+--- 108사원 부서장의 부서는 90번이고 부서명은 영업부이며 사원수는 20명입니다.
+select * from departments where manager_id is not null;
+
+DECLARE
+    deptid NUMBER;
+    deptname VARCHAR2(20);
+    emps NUMBER;
+    mngid NUMBER:= &mngid; 
+BEGIN
+    case mngid 
+        WHEN 200 then 
+            select department_id , department_name 
+            into deptid, deptname
+            from departments
+            where manager_id =  mngid;
+            select count(*) into emps
+            from employees where department_id = deptid;
+        WHEN 201 then 
+            select department_id , department_name 
+            into deptid, deptname
+            from departments
+            where manager_id =  mngid;
+            select count(*) into emps
+            from employees where department_id = deptid;
+        WHEN 114 then 
+            select department_id , department_name 
+            into deptid, deptname
+            from departments
+            where manager_id =  mngid;
+            select count(*) into emps
+            from employees where department_id = deptid;
+     end case;
+     DBMS_OUTPUT.PUT_LINE(
+     mngid || '부서장의 부서는' ||deptid||'번이고 부서명은' ||deptname
+     ||'이며 사원수는 '||emps||'명입니다.');
+end;
+/
+select * from employees where department_id = 10;
 
 
 

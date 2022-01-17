d6f076ed11fff0439f14535475473931c912c8e9
A-13 <A-13@DESKTOP-U8BDI5U>
2022-01-17 16:25:31 +0900

    202201171625

--- springBootTest2/src/main/java/springBootTest2/DataBaseInfo.java
+++ springBootTest2/src/main/java/springBootTest2/DataBaseInfo.java
@@ -5,27 +5,26 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class DataBaseInfo {
 	@Bean
-	public SqlSessionFactory sqlSessionFactory(
-			DataSource dataSource,ApplicationContext applicationContext) throws Exception {
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource, ApplicationContext applicationContext)
+			throws Exception {
 		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
 		sessionFactory.setDataSource(dataSource);
 		sessionFactory.setMapperLocations(applicationContext.getResources("classpath:mappers/**/*.xml"));
 		sessionFactory.setTypeAliasesPackage("springBootTest2.domain");
+		sessionFactory.setConfigLocation(
+				new ClassPathResource("mybatis-config.xml"));
 		return sessionFactory.getObject();
 	}
-
-	  @Bean 
-	  public SqlSessionTemplate sqlSession ( 
-			  SqlSessionFactory sqlSessionFactory) 
-					  throws Exception{ 
-		  return new SqlSessionTemplate(sqlSessionFactory); 
-	  }
+	@Bean
+	public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) throws Exception {
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
 }
--- springBootTest2/src/main/java/springBootTest2/domain/MemberDTO.java
+++ springBootTest2/src/main/java/springBootTest2/domain/MemberDTO.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.type.Alias;
 import lombok.Data;
 
 @Data
-@Alias("memberVO")
+@Alias("memberDTO")
 public class MemberDTO {
 	String memNum; 
 	String memName;

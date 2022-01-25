8942f201d6d7bf847f11fa7486193ac1fc8f95b4
A-13 <A-13@DESKTOP-U8BDI5U>
2022-01-25 16:51:03 +0900

    202201251650

---
+++ kosaShoppingMall/src/main/java/kosaShoppingMall/KosaShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package kosaShoppingMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosaShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosaShoppingMallApplication.class, args);
+	}
+
+}
---
+++ kosaShoppingMall/src/test/java/kosaShoppingMall/KosaShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package kosaShoppingMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosaShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
